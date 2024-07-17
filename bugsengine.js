c414a38c75d9d2cb3a5cc6c4684103dc33abc015
VadymWebC <vadymcdevelweb@gmail.com>
2024-07-17 17:32:15 +0300

    add 1st bug

--- bugsengine.js
+++ bugsengine.js
@@ -7,20 +7,27 @@
     theY: 0,
     theDirection: 0,
     theSpeed: 0,
-    theBugHTMLElement,
+    theBugHTMLElement: null,
     doUpdate: () => {
-      // document.querySelector("#hive")
+      let theCanvas = document.querySelector('#hive')
       let theBugHTMLElement = (bug.theBugHTMLElement =
         document.createElement('div'))
+
+      let { theName, theX, theY, theDirection } = bug
 
       Object.assign(theBugHTMLElement, {
         className: 'bug',
         title: theName,
-        style: `top ${Math.random() * 300 + 50}px;
+        style: `top: ${Math.random() * 300 + 50}px;
                  left: ${Math.random() * 300 + 50}px;
                  transform: rotate(${Math.random() * 360}deg)
                 `,
       })
+
+      theCanvas.appendChild(theBugHTMLElement)
+      //
     },
   }
+
+  bug.doUpdate()
 })()

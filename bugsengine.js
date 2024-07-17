1656e0d485cda7206ed72ee13c0383632872933d
VadymWebC <vadymcdevelweb@gmail.com>
2024-07-17 09:52:14 +0300

    set default pos fot the bug

--- bugsengine.js
+++ bugsengine.js
@@ -2,13 +2,25 @@
   'use strict'
 
   let bug = {
-    name: '',
+    theName: '',
     theX: 0,
     theY: 0,
     theDirection: 0,
     theSpeed: 0,
+    theBugHTMLElement,
     doUpdate: () => {
       // document.querySelector("#hive")
+      let theBugHTMLElement = (bug.theBugHTMLElement =
+        document.createElement('div'))
+
+      Object.assign(theBugHTMLElement, {
+        className: 'bug',
+        title: theName,
+        style: `top ${Math.random() * 300 + 50}px;
+                 left: ${Math.random() * 300 + 50}px;
+                 transform: rotate(${Math.random() * 360}deg)
+                `,
+      })
     },
   }
 })()
